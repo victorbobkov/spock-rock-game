d64be0d7093a41a5bc9ffa207e0e955c3e7a3046
victor bobkov <victorbobkov@gmail.com>
2022-05-11 03:46:13 +0400

    Add computer selection

--- app.js
+++ app.js
@@ -25,6 +25,7 @@ const choices = {
    lizard: {name: 'Lizard', defeats: ['paper', 'spock']},
    spock: {name: 'Spock', defeats: ['scissors', 'rock']},
 }
+let computerChoice = ''
 
 // Reset all 'selected' icons
 const resetSelected = () => {
@@ -33,9 +34,60 @@ const resetSelected = () => {
   })
 }
 
+// Random computer choice
+const computerRandomChoice = () => {
+   const computerChoiceNumber = Math.random()
+   if (computerChoiceNumber < 0.2) {
+      computerChoice = 'rock'
+   } else if (computerChoiceNumber <= 0.4) {
+      computerChoice = 'paper'
+   } else if (computerChoiceNumber <= 0.6) {
+      computerChoice = 'scissors'
+   } else if (computerChoiceNumber <= 0.8) {
+      computerChoice = 'lizard'
+   } else {
+      computerChoice = 'spock'
+   }
+}
+
+// Add 'selected' styling & computerChoice
+const displayComputerChoice = () => {
+   switch (computerChoice) {
+      case 'rock':
+         computerRock.classList.add('selected')
+         computerChoiceEl.textContent = ' --- Rock'
+         break
+      case 'paper':
+         computerPaper.classList.add('selected')
+         computerChoiceEl.textContent = ' --- Paper'
+         break
+      case 'scissors':
+         computerScissors.classList.add('selected')
+         computerChoiceEl.textContent = ' --- Scissors'
+         break
+      case 'lizard':
+         computerLizard.classList.add('selected')
+         computerChoiceEl.textContent = ' --- Lizard'
+         break
+      case 'spock':
+         computerSpock.classList.add('selected')
+         computerChoiceEl.textContent = ' --- Spock'
+         break
+      default:
+         break
+   }
+}
+
+// Call functions to process turn
+const checkResult = () => {
+   resetSelected()
+   computerRandomChoice()
+   displayComputerChoice()
+}
+
 // Passing player selection value and styling icons
 const select = (playerChoice) => {
-   resetSelected()
+   checkResult()
    // Add 'selected' styling & playerChoice
    switch (playerChoice) {
       case 'rock':
@@ -43,19 +95,19 @@ const select = (playerChoice) => {
          playerChoiceEl.textContent = ' --- Rock'
          break
       case 'paper':
-         playerRock.classList.add('selected')
+         playerPaper.classList.add('selected')
          playerChoiceEl.textContent = ' --- Paper'
          break
       case 'scissors':
-         playerRock.classList.add('selected')
+         playerScissors.classList.add('selected')
          playerChoiceEl.textContent = ' --- Scissors'
          break
       case 'lizard':
-         playerRock.classList.add('selected')
+         playerLizard.classList.add('selected')
          playerChoiceEl.textContent = ' --- Lizard'
          break
       case 'spock':
-         playerRock.classList.add('selected')
+         playerSpock.classList.add('selected')
          playerChoiceEl.textContent = ' --- Spock'
          break
       default:
